4c389eaf2d8aaaf8352233dc054c7817f5465441
juan.moreno <juan.moreno@moelia.com>
2015-12-12 16:31:53 +0100

    Adding addition, substraction and scalar operations

--- linear_algebra_module/vector.js
+++ linear_algebra_module/vector.js
@@ -5,29 +5,67 @@ function Vector(coordinates) {
   if (!coordinates) {
     throw new Error("The coordinates must be nonempty");
   }
-  // if (coordinates.constructor === Array) {
-  //   throw new Error("The coordinates must be an iterable");
-  // }
   this.coordinates = coordinates;
   this.dimension = coordinates.length;
 }
 
-Vector.prototype.add = function() {
+/**
+* Vectors addition
+*/
+Vector.prototype.add = function(vector) {
+  if (this.coordinates.length !== vector.coordinates.length) {
+    throw new Error("Vectors should have same length");
+  }
+  var result = [];
 
+  for (var i = 0; i < this.coordinates.length; i++) {
+    result.push(this.coordinates[i] + vector.coordinates[i]);
+  }
+
+  return new Vector(result);
 }
 
-Vector.prototype.substract = function() {
+/*
+* Vectos substraction
+*/
+Vector.prototype.substract = function(vector) {
+  if (this.coordinates.length !== vector.coordinates.length) {
+    throw new Error("Vectors should have same length");
+  }
+  var result = [];
 
+  for (var i = 0; i < this.coordinates.length; i++) {
+    result.push(this.coordinates[i] - vector.coordinates[i]);
+  }
+  return new Vector(result);
 }
 
+/*
+* Vector scalar multiplication
+*/
 Vector.prototype.multiply = function() {
+  if (this.coordinates.length !== vector.coordinates.length) {
+    throw new Error("Vectors should have same length");
+  }
+  var result = [];
 
+  for (var i = 0; i < this.coordinates.length; i++) {
+    result.push(this.coordinates[i] * vector.coordinates[i]);
+  }
+
+  return new Vector(result);
 }
 
+/**
+* Vector string representation
+*/
 Vector.prototype.toString = function() {
   return "Vector: " + coordinates;
 }
 
+/**
+* Check if vector coordinates are equal
+*/
 Vector.prototype.isEqual = function(vector) {
   if (this.coordinates.length !== vector.coordinates.length) {
     return false;
@@ -47,3 +85,5 @@ my_vector3 = new Vector([-1,2,3]);
 
 console.log(my_vector.isEqual(my_vector2));
 console.log(my_vector.isEqual(my_vector3));
+
+console.log(my_vector.substract(my_vector3));
