909e60e26a3b6d842c515e21ab13b63ad9b4abeb
juan moreno <juan.moreno@moelia.com>
2015-12-16 16:22:47 +0100

    Vector class es6 first approach

--- linear_algebra_module/vector.js
+++ linear_algebra_module/vector.js
@@ -1,87 +1,90 @@
+"use strict";
+
 /**
 * Vector object
 */
-function Vector(coordinates) {
-  if (!coordinates) {
-    throw new Error("The coordinates must be nonempty");
-  }
-  this.coordinates = coordinates;
-  this.dimension = coordinates.length;
-}
+class Vector {
 
-/**
-* Vectors addition
-*/
-Vector.prototype.add = function(vector) {
-  if (this.coordinates.length !== vector.coordinates.length) {
-    throw new Error("Vectors should have same length");
-  }
-  var result = [];
-
-  for (var i = 0; i < this.coordinates.length; i++) {
-    result.push(this.coordinates[i] + vector.coordinates[i]);
+  constructor(coordinates) {
+    this.coordinates = coordinates;
+    this.dimension = coordinates.length;
   }
 
-  return new Vector(result);
-}
+  /*
+  * Vectos substraction
+  */
+  add(vector) {
+    if (this.coordinates.length !== vector.coordinates.length) {
+      throw new Error("Vectors should have same length");
+    }
+    var result = [];
 
-/*
-* Vectos substraction
-*/
-Vector.prototype.substract = function(vector) {
-  if (this.coordinates.length !== vector.coordinates.length) {
-    throw new Error("Vectors should have same length");
-  }
-  var result = [];
+    for (var i = 0; i < this.coordinates.length; i++) {
+      result.push(this.coordinates[i] + vector.coordinates[i]);
+    }
 
-  for (var i = 0; i < this.coordinates.length; i++) {
-    result.push(this.coordinates[i] - vector.coordinates[i]);
-  }
-  return new Vector(result);
-}
-
-/*
-* Vector scalar multiplication
-*/
-Vector.prototype.multiply = function() {
-  if (this.coordinates.length !== vector.coordinates.length) {
-    throw new Error("Vectors should have same length");
-  }
-  var result = [];
-
-  for (var i = 0; i < this.coordinates.length; i++) {
-    result.push(this.coordinates[i] * vector.coordinates[i]);
+    return new Vector(result);
   }
 
-  return new Vector(result);
-}
+  /*
+  * Vectos substraction
+  */
+  substract(vector) {
+    if (this.coordinates.length !== vector.coordinates.length) {
+      throw new Error("Vectors should have same length");
+    }
+    var result = [];
 
-/**
-* Vector string representation
-*/
-Vector.prototype.toString = function() {
-  return "Vector: " + coordinates;
-}
-
-/**
-* Check if vector coordinates are equal
-*/
-Vector.prototype.isEqual = function(vector) {
-  if (this.coordinates.length !== vector.coordinates.length) {
-    return false;
+    for (var i = 0; i < this.coordinates.length; i++) {
+      result.push(this.coordinates[i] - vector.coordinates[i]);
+    }
+    return new Vector(result);
   }
-  for (var i = this.coordinates.length; i--;) {
-    if (this.coordinates[i] !== vector.coordinates[i]) {
+
+  /*
+  * Vector scalar multiplication
+  */
+  multiply() {
+    if (this.coordinates.length !== vector.coordinates.length) {
+      throw new Error("Vectors should have same length");
+    }
+    var result = [];
+
+    for (var i = 0; i < this.coordinates.length; i++) {
+      result.push(this.coordinates[i] * vector.coordinates[i]);
+    }
+
+    return new Vector(result);
+  }
+
+  /**
+  * Vector string representation
+  */
+  toString() {
+    return "Vector: " + coordinates;
+  }
+
+  /**
+  * Check if vector coordinates are equal
+  */
+  isEqual(vector) {
+    if (this.coordinates.length !== vector.coordinates.length) {
       return false;
     }
+    for (var i = this.coordinates.length; i--;) {
+      if (this.coordinates[i] !== vector.coordinates[i]) {
+        return false;
+      }
+    }
+    return true;
   }
-  return true;
+
 }
 
-my_vector = new Vector([1,2,3]);
+let my_vector = new Vector([1,2,3]);
 console.log(my_vector);
-my_vector2 = new Vector([1,2,3]);
-my_vector3 = new Vector([-1,2,3]);
+let my_vector2 = new Vector([1,2,3]);
+let my_vector3 = new Vector([-1,2,3]);
 
 console.log(my_vector.isEqual(my_vector2));
 console.log(my_vector.isEqual(my_vector3));
